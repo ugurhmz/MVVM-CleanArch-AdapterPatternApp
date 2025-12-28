21aa9b409fca4cb08b6fe756b37ddf15161630cd
Uğur hamzaoğlu <ugur.hmz52@gmail.com>
2025-12-29 02:35:29 +0300

    init

---
+++ MVVM-CleanArch-AdapterPatternApp/Application/AppFactory.swift
@@ -0,0 +1,8 @@
+//
+//  AppFactory.swift
+//  MVVM-CleanArch-AdapterPatternApp
+//
+//  Created by rico on 29.12.2025.
+//
+
+import Foundation
---
+++ MVVM-CleanArch-AdapterPatternApp/Presentation/Scenes/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  MVVM-CleanArch-AdapterPatternApp
+//
+//  Created by rico on 29.12.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
